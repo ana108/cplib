fb1494787098a7476f1146fdff7d070465c7da21
ana108 <flutegurl108@hotmail.com>
2021-03-15 22:24:07 -0400

    test and verify

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,11 @@
-//import { calculateShippingCost } from "/home/ana/GitHub/cplib/src/calculate";
-
 import fs from 'fs';
 import readline from 'readline';
 import { once } from 'events';
 import sqlite3 from 'sqlite3';
 
-/*exports.calculateShippingCost = function (sourceAddress: any, destinationAddress: any, packageDetails: any): any {
-    return calculateShippingCost(sourceAddress, destinationAddress, packageDetails);
-}*/
-//exports.calculateShippingCost = calculateShippingCost;
 let postalData: sqlite3.Database;
-// const db_name = 'C:/Users/flute/Documents/GitHub/cplib/resources/cplib2.db';
-const db_name = '/home/ana/GitHub/cplib/resources/cplib1.db';
-
+// TODO figure out how to make this relative no matter what package calls it
+const db_name = '/home/ana/GitHub/cplib/resources/cplib.db';
 // for unit testing; stub database
 export const setDB = function (anyNewDB: any): void {
     postalData = anyNewDB;
@@ -20,7 +13,6 @@ export const setDB = function (anyNewDB: any): void {
 
 export const writeToDB = async function (sqlStmt: string): Promise<string> {
     const saveToDb = async function (resolve, reject) {
-        console.log('SQL STMT ' + sqlStmt);
         const stmt = postalData.prepare(sqlStmt);
         stmt.run([], (err: Error) => {
             if (err) {
@@ -33,7 +25,6 @@ export const writeToDB = async function (sqlStmt: string): Promise<string> {
     }
     const prom = new Promise<string>((resolve, reject) => {
         if (!postalData) {
-            console.log('Open Database');
             postalData = new sqlite3.Database(db_name, sqlite3.OPEN_READWRITE, (err: any) => {
                 if (err) {
                     console.log(err);
@@ -62,14 +53,12 @@ export const readFile = async function (fileName: string, type: string, year: nu
             isFirst = false;
         } else {
             const tokens = input.split(' ');
-            console.log('INPUT Exepct rate code to be token')
             const maxWeight = tokens[0];
             for (let i = 2; i < labels.length; i++) {
                 const price = tokens[i];
                 const rate_code = labels[i];
-                console.log('Rate Code ' + rate_code);
                 const insertDataSQL = `insert into RATES(year, max_weight, weight_type, rate_code, price, type, country, customer_type)
-                 VALUES(${year}, ${maxWeight}, 'kg', '${rate_code}', ${price}, ${type}, 'Canada', ${customer_type})`;
+                 VALUES(${year}, ${maxWeight}, 'kg', '${rate_code}', ${price}, '${type}', 'Canada', '${customer_type}')`;
                 inputsAll.push(insertDataSQL);
             }
         }
@@ -88,10 +77,13 @@ export const readFile = async function (fileName: string, type: string, year: nu
 
 export const files = async function (): Promise<any> {
     const YEAR = new Date().getFullYear();
-    const regular_rate_base_dir = `../resources/regular/${YEAR}/`;
+    const regular_rate_base_dir = `${__dirname}/../resources/regular/${YEAR}/`;
     let dataPrepared = fs.existsSync(regular_rate_base_dir)
     if (!dataPrepared) {
+        console.log('Could not find the directory ' + regular_rate_base_dir);
         return `Failed to find directory ${regular_rate_base_dir} Please prepare data for year ${YEAR} or set the year variable to previous year`;
+    } else {
+        console.log('Directory exists. Continue');
     }
     const FILES = {
         'express': ['express_canada_1.txt', 'express_canada_2.txt'],
@@ -100,27 +92,26 @@ export const files = async function (): Promise<any> {
     };
 
     await Promise.all(Object.keys(FILES).map(async fileType => {
-        FILES[fileType].forEach(fileName => {
-            const filePath = __dirname + '/' + regular_rate_base_dir + fileName;
+        return FILES[fileType].forEach(fileName => {
+            const filePath = regular_rate_base_dir + fileName;
             return module.exports.readFile(filePath, fileType, YEAR, 'regular');            
         });
 
     }));
     
-    const small_business_base_dir = `../resources/small_business/${YEAR}/`;
+    const small_business_base_dir =  `${__dirname}/../resources/small_business/${YEAR}/`;
     dataPrepared = fs.existsSync(small_business_base_dir)
     if (!dataPrepared) {
         return `Failed to find directory ${small_business_base_dir} Please prepare data for year ${YEAR} or set the year variable to previous year`;
     }
 
     await Promise.all(Object.keys(FILES).map(async fileType => {
-        FILES[fileType].forEach(fileName => {
-            const filePath = __dirname + '/' + small_business_base_dir + fileName;
+        return FILES[fileType].forEach(fileName => {
+            const filePath = small_business_base_dir + fileName;
             return module.exports.readFile(filePath, fileType, YEAR, 'small_business');            
         });
 
     }));
 
     return `Successfully loaded the data for year ${YEAR}`;
-
 }
