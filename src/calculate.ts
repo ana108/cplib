1c1c9b8ed84f61cefdfc524a4803f62a3f895b0c
ana108 <flutegurl108@hotmail.com>
2021-03-25 12:22:32 -0400

    calculated canadian shipping rate without tax

--- src/calculate.ts
+++ src/calculate.ts
@@ -1,10 +1,10 @@
-import { getRateCode } from './db/sqlite3';
+import { getRateCode, getRate, getProvince, getFuelSurcharge } from './db/sqlite3';
 interface PackageDimensions {
     lengthInCM: number,
     widthInCM: number,
     heightInCM: number,
     weightInGrams: number,
-    pageCount: number 
+    pageCount: number
 };
 interface ShippingDetails {
     bookDimensions: PackageDimensions,
@@ -14,35 +14,49 @@ interface ShippingDetails {
 }
 const shippingInformation = {
     bookDimensions: {
-      lengthInCM: 22.3,
-      widthInCM: 15.2,
-      heightInCM: 1.2,
-      weightInGrams: 966,
-      pageCount: 216,
+        lengthInCM: 22.3,
+        widthInCM: 15.2,
+        heightInCM: 1.2,
+        weightInGrams: 966,
+        pageCount: 216,
     },
     sellerCountry: 'CA',
     sellerPostalCode: 'J9H5V8',
     buyerPostalCode: 'K1G0E6',
-  };
+};
+
+
+function calculateTax(sourceProvince, shippingCost): number {
+    return 0;
+}
 export function calculateShippingCost(packageDimensions: ShippingDetails): Promise<any> {
     console.log('Calculate Shipping Cost');
-    return new Promise<any> (async (resolve, reject) => {
-        // truncate source and destination to be the first 3 chars
+    return new Promise<any>(async (resolve, reject) => {
         // for package dimensions make sure to convert it into a type
         try {
             // get rate code
-            let rateCode = await getRateCode(packageDimensions.sellerPostalCode, packageDimensions.buyerPostalCode);
+            const rateCode = await getRateCode(packageDimensions.sellerPostalCode, packageDimensions.buyerPostalCode);
 
             // get weight on kg
-            let weightInKg = packageDimensions.bookDimensions.weightInGrams/1000;
-            // get cost for priority
-            // get province of source
-            // calculate final cost
+            const weightInKg = packageDimensions.bookDimensions.weightInGrams / 1000;
+            // get cost for regular/priority/express
+            const shippingCost = await getRate(rateCode, weightInKg);
             // get fuel rate
+            const fuelSurchargePercentage = await getFuelSurcharge();
+            console.log('Fuel Surcharge Returned ' + fuelSurchargePercentage);
             // add fuel rate to final cost
-        } catch(e) {
-            console.log('Error! ' + JSON.stringify(e));
+            const pretaxCost = shippingCost * (1 + fuelSurchargePercentage);
+            // get province of source for tax purposes
+            const sourceProvince = await getProvince(packageDimensions.sellerPostalCode);
+            console.log('Source Province ' + sourceProvince);
+            const finalPrice = calculateTax(sourceProvince, pretaxCost);
+            resolve(round(pretaxCost));
+        } catch (e) {
+            reject(e);
         }
-
     });
+}
+// math utility function
+function round(num: number): number {
+    return Math.round(num * 100 + Number.EPSILON) / 100
 }
--- src/db/sqlite3.ts
+++ src/db/sqlite3.ts
@@ -1,45 +1,115 @@
 import sqlite3 from 'sqlite3';
 
-const dbname = __dirname+"/../../resources/cplib.db";
+const dbname = __dirname + "/../../resources/cplib.db";
 const db = new sqlite3.Database(dbname, sqlite3.OPEN_READWRITE);
+
 export function getRateCode(sourcePostalCode: string, destinationPostalCode: string): Promise<any> {
   let source = sourcePostalCode.substr(0, 3);
   let destination = destinationPostalCode.substr(0, 3);
 
-  return new Promise(function(resolve, reject){
+  return new Promise(function (resolve, reject) {
     let getRateCodeMapping = `select rate_code from rate_code_mapping where source ='${source}' and destination like '%${destination}%'`;
-        db.get(getRateCodeMapping, [], (err, row) => {
-            if (err) {
-              reject(err);
-            } else {
-              resolve(row["rate_code"]);
-            }
-        })
-    });
-  
+    db.get(getRateCodeMapping, [], (err, row) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(row["rate_code"]);
+      }
+    })
+  });
+
 }
 
 export function saveToDb(sqlStmt): Promise<any> {
   return new Promise((resolve, reject) => {
     const stmt = db.prepare(sqlStmt);
     stmt.run([], (err: Error) => {
-        if (err) {
-            console.log('Error! ' + err.message);
-            reject(err.message);
-        } else {
-            resolve('Success');
-        }
+      if (err) {
+        reject(err.message);
+      } else {
+        resolve('Success');
+      }
     });
   });
 }
 
 interface options {
-  country?: string,
-  weight_type?: string,
-  type?: string,
+  country?: string
+  weight_type?: string
+  type?: 'regular' | 'priority' | 'express'
+  customerType?: 'regular' | 'small_business'
   year?: number
 }
-export function getRate(rateCode: string, weight: number, opts: options = { country: 'Canada', weight_type: 'kg', type: 'regular', year: new Date().getFullYear()}) {
-    let getPrice = 'select price from rates where country = ? and rate_code = ? and max_weight > ? ';
+export function getRate(rateCode: string, weight: number,
+  opts: options = { country: 'Canada', weight_type: 'kg', type: 'regular', customerType: 'regular', year: new Date().getFullYear() }): Promise<number> {
+  const getPrice = 'select price from rates where country = $country and rate_code = $rateCode and max_weight >= $weight and year = $year ' +
+    'and type = $deliverySpeed and customer_type = $customerType group by(rate_code) having min(price)';
+
+  return new Promise<number>((resolve, reject) => {
+    const stmt = db.prepare(getPrice)
+    stmt.get({
+      $country: opts.country,
+      $rateCode: rateCode,
+      $weight: weight,
+      $year: opts.year,
+      $deliverySpeed: opts.type,
+      $customerType: opts.customerType
+    }, (err, row) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(parseFloat(row.price));
+      }
+    });
+  });
+
 }
 
+export function getProvince(postalCode: string): Promise<string> {
+  const getProvince = 'select province from postal_codes where postal_code = $postalCode';
+  return new Promise<string>((resolve, reject) => {
+    const stmt = db.prepare(getProvince);
+    stmt.get({
+      $postalCode: postalCode
+    }, (err, row) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(row.province);
+      }
+    })
+  });
+}
+
+export function updateFuelSurcharge(percentage: number): Promise<void> {
+  const addFuelSurcharge = 'insert into fuel_surcharge values($percentage, strftime(\'%s\', \'now\'));';
+  return new Promise<void>((resolve, reject) => {
+    if (percentage < 0.0 || percentage > 100.0) {
+      reject('Percentage must be specified between 0.00 and 99.99');
+    }
+    const stmt = db.prepare(addFuelSurcharge);
+    stmt.run({
+      $percentage: percentage
+    }, (err) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve();
+      }
+    })
+  });
+}
+
+export function getFuelSurcharge(): Promise<number> {
+  const getLatestFuelSurcharge = 'select percentage from fuel_surcharge order by date desc limit 1';
+  return new Promise<number>((resolve, reject) => {
+    const stmt = db.prepare(getLatestFuelSurcharge);
+    stmt.get([], (err, row) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(parseFloat(row.percentage));
+      }
+    });
+  })
+}
