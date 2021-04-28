a3528ce199af275190f5f60dff446f38ef546210
ana108 <flutegurl108@hotmail.com>
2021-04-28 09:07:58 -0400

    work in progress for integration tests

--- src/integration/integration.test.ts
+++ src/integration/integration.test.ts
@@ -10,19 +10,43 @@ import * as db from '../db/sqlite3';
 import * as chai from 'chai';
 import { fail } from 'assert';
 import { maxRates } from '../db/sqlite3';
+import { allTestCases } from './testcases';
 
 const expect = chai.expect;
-db.setDB(__dirname + "/cplib_2021_int.db");
 
-describe('Calculate Shipping Cost By Postal Code', () => {
+describe('Table Tests - Canada Regular Parcel - 0.75 - 2.5kg', () => {
 
     beforeEach(() => {
-
+        console.log('Setting DB to: ' + __dirname + "/cplib_2021_int.db");
+        db.setDB(__dirname + "/cplib_2021_int.db");
     });
     afterEach(() => {
-
+        //db.resetDB();
     });
-
+    let numKeys = Object.keys(allTestCases).length;
+    let sourceAddr = {
+        streetAddress: '812 Terravita Pvt',
+        city: 'Ottawa',
+        region: 'ON',
+        postalCode: 'K1V2R9',
+        country: 'CA'
+    };
+    let destinationAddr = {
+        streetAddress: '115 Prentiss Rue',
+        city: 'New York',
+        region: 'ON',
+        postalCode: 'J9H5V8',
+        country: 'CA'
+    };
+    for (let i = 0; i < 1; i++) {
+        let rateCode = Object.keys(allTestCases)[0];
+        it(rateCode, async () => {
+            sourceAddr.postalCode = allTestCases[rateCode].postalCodes.src;
+            destinationAddr.postalCode = allTestCases[rateCode].postalCodes.dest;
+            let result = await calculateShipping(sourceAddr, destinationAddr, 0.75, 'regular', 'small_business');
+            expect(result).to.equal(allTestCases[rateCode].weights['0.75']);
+        });
+    }
     it.skip('A5 - Regular - 0.7kg - 10.89', async () => {
 
     });
